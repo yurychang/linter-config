7969c8686f7a7143b1073a26ed6da1fdaeddf303
Yury <yuury.chang@gmail.com>
2023-10-02 15:17:38 +0800

    Update my-script.sh

--- my-script.sh
+++ my-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "version=github.event.head_commit.mesasge" >> "$GITHUB_ENV"
+echo "version=github.event.head_commit.message" >> "$GITHUB_ENV"
 printf "$COMMIT_MESSAGE"
 printf "$version"
