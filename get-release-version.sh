2940f877b8cecd0a3adf7969ee8be9499b999866
Yury <yuury.chang@gmail.com>
2023-10-02 17:21:11 +0800

    [eslint-config] v0.43.55

--- get-release-version.sh
+++ get-release-version.sh
@@ -2,7 +2,7 @@
 commit_regex="\[eslint-config\] v[0-9]+\.[0-9]+\.[0-9]+"
 if [[ $commit_message =~ $commit_regex ]]
 then
-  release_version=${commit_message:24}
+  release_version=${commit_message:16}
   echo "release_version=$release_version" >> "$GITHUB_ENV"
 else
   echo "::error incorrect version of commit message"
