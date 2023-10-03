99c501604d7695c6ebe81e69dab12d57d5e5906a
Yury <yuury.chang@gmail.com>
2023-10-03 23:07:27 +0800

    [eslint-config] v0.4.ad

--- get-release-version.sh
+++ get-release-version.sh
@@ -5,5 +5,6 @@ then
   release_version=${commit_message:16}
   echo "release_version=$release_version" >> "$GITHUB_ENV"
 else
-  echo "::error incorrect version of commit message"
+  echo "Error: incorrect version of commit message"
+  exit 1
 fi
