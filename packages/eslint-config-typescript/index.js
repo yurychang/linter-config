2517859c79d797d05734dd3332b2206fa3e9269b
Peter <yuury.chang@gmail.com>
2023-10-11 10:31:51 +0800

    Update ts lint

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -13,6 +13,11 @@ module.exports = {
         // typescript-eslint specific options
         warnOnUnsupportedTypeScriptVersion: true,
       },
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-unsafe-member-access': 'warn',
+      },
     },
   ],
 };
